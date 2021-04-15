6506b27228296fbcf64255100ac16d1413478179
mrizkyff <muhamadrizkyff@gmail.com>
2021-04-15 14:08:19 +0700

    routing dengan react native navigation

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../pages/Home';
+import Splash from '../pages/Splash';
+
+const Stack = createStackNavigator();
+
+const Router = () => {
+    return (
+        <Stack.Navigator initialRouteName="Splash">
+            <Stack.Screen name="Splash" component={Splash} options={{ headerShown:false }}/>
+            <Stack.Screen name="Home" component={Home} options={{ headerShown:false }}/>
+        </Stack.Navigator>
+    )
+}
+
+export default Router
+
+const styles = StyleSheet.create({})
